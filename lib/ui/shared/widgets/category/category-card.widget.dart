66493ab555f9de7bb216224404935df26407716e
thiago-miranda <thiago.miranda@hotmail.com>
2022-05-09 22:22:47 -0300

    Category Card Widget

---
+++ lib/ui/shared/widgets/category/category-card.widget.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:shopping/blocs/home.bloc.dart';
+import 'package:shopping/models/category-list-item.model.dart';
+import 'package:provider/provider.dart';
+import '../../../../settings.dart';
+
+class CategoryCard extends StatelessWidget {
+  final CategoryListItemModel item;
+
+  const CategoryCard({Key? key, required this.item}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final HomeBloc bloc = Provider.of<HomeBloc>(context);
+
+    return Container(
+      height: 70,
+      width: 70,
+      margin: const EdgeInsets.all(5),
+      padding: const EdgeInsets.all(5),
+      decoration: BoxDecoration(
+        color: item.tag == bloc.selectedCategory
+            ? Theme.of(context).primaryColor.withOpacity(0.3)
+            : Theme.of(context).primaryColor,
+        borderRadius: const BorderRadius.all(
+          Radius.circular(70),
+        ),
+      ),
+      child: TextButton(
+        child: Image.asset(
+          "assets/categories/${Settings.theme}/${item.tag}.png",
+        ),
+        onPressed: () {
+          bloc.changeCategory(item.tag);
+        },
+      ),
+    );
+  }
+}
