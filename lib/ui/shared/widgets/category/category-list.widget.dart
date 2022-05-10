7bc682c12faf5798f2fa2f27c8d5db20bdec342c
thiago-miranda <thiago.miranda@hotmail.com>
2022-05-09 22:58:57 -0300

    Carregando as Categorias: Parte 2

--- lib/ui/shared/widgets/category/category-list.widget.dart
+++ lib/ui/shared/widgets/category/category-list.widget.dart
@@ -1,3 +1,7 @@
+import 'dart:js';
+
+import 'package:shopping/ui/shared/widgets/shared/loader.widget.dart';
+
 import '../../../../models/category-list-item.model.dart';
 import 'category-card.widget.dart';
 import 'package:flutter/material.dart';
@@ -10,6 +14,25 @@ class CategoryList extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Container(
+      height: 90,
+      child: Loader(object: categories, callback: list),
+    );
+  }
+
+  Widget list() {
+    return ListView.builder(
+      scrollDirection: Axis.horizontal,
+      itemCount: categories.length,
+      itemBuilder: (context, index) {
+        CategoryListItemModel item = categories[index];
+        return Padding(
+          padding: const EdgeInsets.all(5),
+          child: CategoryCard(
+            item: item,
+          ),
+        );
+      },
+    );
   }
 }
---
+++ lib/ui/shared/widgets/shared/loader.widget.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/widgets.dart';
+
+import '../shared/progress-indicator.widget.dart';
+
+class Loader extends StatelessWidget {
+  final object;
+  final Function callback;
+  const Loader({Key? key, required this.object, required this.callback})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    if (object == null) {
+      return const Center(
+        child: GenericProgressIndicator(),
+      );
+    }
+    if (object.length == 0) {
+      return const Center(
+        child: Text("Nenhum item encontrado"),
+      );
+    }
+    return callback();
+  }
+}
